2c8ce8f18b7f891c6815b44f21d94ec2bcd4ae8f
Thatsmusic99 <tmtherobotcat@gmail.com>
2019-07-07 16:28:40 +0100

    Create pl_pl.java

---
+++ MAIN/src/main/java/io/github/thatsmusic99/headsplus/locale/pl_pl.java
@@ -0,0 +1,633 @@
+package io.github.thatsmusic99.headsplus.locale;
+
+public class pl_pl implements Locale {
+    @Override
+    public boolean active() {
+        return false;
+    }
+
+    @Override
+    public String getLanguage() {
+        return null;
+    }
+
+    @Override
+    public String getReloadingMessage() {
+        return null;
+    }
+
+    @Override
+    public String getReloadMessage() {
+        return null;
+    }
+
+    @Override
+    public String getHeadInteractMessage() {
+        return null;
+    }
+
+    @Override
+    public String getHeadMhfInteractMessage() {
+        return null;
+    }
+
+    @Override
+    public String getHeadMhfInteractMessage2() {
+        return null;
+    }
+
+    @Override
+    public String getSellSuccess() {
+        return null;
+    }
+
+    @Override
+    public String getNotEnoughHeads() {
+        return null;
+    }
+
+    @Override
+    public String getNoHeads() {
+        return null;
+    }
+
+    @Override
+    public String getInvalidArguments() {
+        return null;
+    }
+
+    @Override
+    public String getFalseHead() {
+        return null;
+    }
+
+    @Override
+    public String getFalseItem() {
+        return null;
+    }
+
+    @Override
+    public String getBlacklistHead() {
+        return null;
+    }
+
+    @Override
+    public String getWhitelistHead() {
+        return null;
+    }
+
+    @Override
+    public String getFullInventory() {
+        return null;
+    }
+
+    @Override
+    public String getAlphaNames() {
+        return null;
+    }
+
+    @Override
+    public String getTooManyArguments() {
+        return null;
+    }
+
+    @Override
+    public String getHeadTooLong() {
+        return null;
+    }
+
+    @Override
+    public String getHeadTooShort() {
+        return null;
+    }
+
+    @Override
+    public String getInvalidPageNumber() {
+        return null;
+    }
+
+    @Override
+    public String getInvalidInputInteger() {
+        return null;
+    }
+
+    @Override
+    public String getNoPermissions() {
+        return null;
+    }
+
+    @Override
+    public String getHeadAlreadyAdded() {
+        return null;
+    }
+
+    @Override
+    public String getHeadAddedBlacklist() {
+        return null;
+    }
+
+    @Override
+    public String getHeadNotOnBlacklist() {
+        return null;
+    }
+
+    @Override
+    public String getHeadRemovedBlacklist() {
+        return null;
+    }
+
+    @Override
+    public String getHeadAddedWhitelist() {
+        return null;
+    }
+
+    @Override
+    public String getHeadNotOnWhitelist() {
+        return null;
+    }
+
+    @Override
+    public String getHeadRemovedWhitelist() {
+        return null;
+    }
+
+    @Override
+    public String getWorldAlreadyAdded() {
+        return null;
+    }
+
+    @Override
+    public String getWorldAddedBlacklist() {
+        return null;
+    }
+
+    @Override
+    public String getWorldNotOnBlacklist() {
+        return null;
+    }
+
+    @Override
+    public String getWorldRemovedBlacklist() {
+        return null;
+    }
+
+    @Override
+    public String getWorldAddedWhitelist() {
+        return null;
+    }
+
+    @Override
+    public String getWorldNotOnWhitelist() {
+        return null;
+    }
+
+    @Override
+    public String getWorldRemovedWhitelist() {
+        return null;
+    }
+
+    @Override
+    public String getBlacklistOn() {
+        return null;
+    }
+
+    @Override
+    public String getBlacklistAlreadyOn() {
+        return null;
+    }
+
+    @Override
+    public String getBlacklistOff() {
+        return null;
+    }
+
+    @Override
+    public String getBlacklistAlreadyOff() {
+        return null;
+    }
+
+    @Override
+    public String getWBlacklistOn() {
+        return null;
+    }
+
+    @Override
+    public String getWBlacklistAlreadyOn() {
+        return null;
+    }
+
+    @Override
+    public String getWBlacklistOff() {
+        return null;
+    }
+
+    @Override
+    public String getWBlacklistAlreadyOff() {
+        return null;
+    }
+
+    @Override
+    public String getWhitelistOn() {
+        return null;
+    }
+
+    @Override
+    public String getWhitelistAlreadyOn() {
+        return null;
+    }
+
+    @Override
+    public String getWhitelistOff() {
+        return null;
+    }
+
+    @Override
+    public String getWhitelistAlreadyOff() {
+        return null;
+    }
+
+    @Override
+    public String getWWhitelistOn() {
+        return null;
+    }
+
+    @Override
+    public String getWWhitelistAlreadyOn() {
+        return null;
+    }
+
+    @Override
+    public String getWWhitelistOff() {
+        return null;
+    }
+
+    @Override
+    public String getWWhitelistAlreadyOff() {
+        return null;
+    }
+
+    @Override
+    public String getDisabledCommand() {
+        return null;
+    }
+
+    @Override
+    public String getEmptyBlacklist() {
+        return null;
+    }
+
+    @Override
+    public String getEmptyWBlacklist() {
+        return null;
+    }
+
+    @Override
+    public String getEmptyWhitelist() {
+        return null;
+    }
+
+    @Override
+    public String getEmptyWWhitelist() {
+        return null;
+    }
+
+    @Override
+    public String getBuySuccess() {
+        return null;
+    }
+
+    @Override
+    public String descBlacklistAdd() {
+        return null;
+    }
+
+    @Override
+    public String descBlacklistDelete() {
+        return null;
+    }
+
+    @Override
+    public String descBlacklistList() {
+        return null;
+    }
+
+    @Override
+    public String descBlacklistToggle() {
+        return null;
+    }
+
+    @Override
+    public String descBlacklistwAdd() {
+        return null;
+    }
+
+    @Override
+    public String descBlacklistwDelete() {
+        return null;
+    }
+
+    @Override
+    public String descBlacklistwList() {
+        return null;
+    }
+
+    @Override
+    public String descBlacklistwToggle() {
+        return null;
+    }
+
+    @Override
+    public String descInfo() {
+        return null;
+    }
+
+    @Override
+    public String descMCReload() {
+        return null;
+    }
+
+    @Override
+    public String descWhitelistAdd() {
+        return null;
+    }
+
+    @Override
+    public String descWhitelistDelete() {
+        return null;
+    }
+
+    @Override
+    public String descWhitelistList() {
+        return null;
+    }
+
+    @Override
+    public String descWhitelistToggle() {
+        return null;
+    }
+
+    @Override
+    public String descWhitelistwAdd() {
+        return null;
+    }
+
+    @Override
+    public String descWhitelistwDelete() {
+        return null;
+    }
+
+    @Override
+    public String descWhitelistwList() {
+        return null;
+    }
+
+    @Override
+    public String descWhitelistwToggle() {
+        return null;
+    }
+
+    @Override
+    public String descHead() {
+        return null;
+    }
+
+    @Override
+    public String descSellhead() {
+        return null;
+    }
+
+    @Override
+    public String descHeads() {
+        return null;
+    }
+
+    @Override
+    public String descMyHead() {
+        return null;
+    }
+
+    @Override
+    public String descHPLeaderboards() {
+        return null;
+    }
+
+    @Override
+    public String getUpdateFound() {
+        return null;
+    }
+
+    @Override
+    public String getCurrentVersion() {
+        return null;
+    }
+
+    @Override
+    public String getNewVersion() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public String getChristmasDeniedMessage() {
+        return null;
+    }
+
+    @Override
+    public String getBlockPlaceDenied() {
+        return null;
+    }
+
+    @Override
+    public String getNoDataRecorded() {
+        return null;
+    }
+
+    @Override
+    public String getPlayerOffline() {
+        return null;
+    }
+
+    @Override
+    public String easy() {
+        return null;
+    }
+
+    @Override
+    public String medium() {
+        return null;
+    }
+
+    @Override
+    public String hard() {
+        return null;
+    }
+
+    @Override
+    public String getReward() {
+        return null;
+    }
+
+    @Override
+    public String chCompleteMessage() {
+        return null;
+    }
+
+    @Override
+    public String descProfile() {
+        return null;
+    }
+
+    @Override
+    public String noData() {
+        return null;
+    }
+
+    @Override
+    public String cantCompleteChallenge() {
+        return null;
+    }
+
+    @Override
+    public String alreadyCompleted() {
+        return null;
+    }
+
+    @Override
+    public String cantViewData() {
+        return null;
+    }
+
+    @Override
+    public String descHelpMenu() {
+        return null;
+    }
+
+    @Override
+    public String descChallenges() {
+        return null;
+    }
+
+    @Override
+    public String descAddHead() {
+        return null;
+    }
+
+    @Override
+    public String getAchievedNextLevel() {
+        return null;
+    }
+
+    @Override
+    public String getPluginUpToDate() {
+        return null;
+    }
+
+    @Override
+    public String getEnabled() {
+        return null;
+    }
+
+    @Override
+    public String getErrorEnabled() {
+        return null;
+    }
+
+    @Override
+    public String getDisabled() {
+        return null;
+    }
+
+    @Override
+    public String badTheme() {
+        return null;
+    }
+
+    @Override
+    public String getCommandFail() {
+        return null;
+    }
+
+    @Override
+    public String noVault() {
+        return null;
+    }
+
+    @Override
+    public String descDebug() {
+        return null;
+    }
+
+    @Override
+    public String noVaultGroup() {
+        return null;
+    }
+
+    @Override
+    public String noNameData() {
+        return null;
+    }
+
+    @Override
+    public String noLoreData() {
+        return null;
+    }
+
+    @Override
+    public String noMaskData() {
+        return null;
+    }
+
+    @Override
+    public String descHeadView() {
+        return null;
+    }
+
+    @Override
+    public String setValue() {
+        return null;
+    }
+
+    @Override
+    public String addedValue() {
+        return null;
+    }
+
+    @Override
+    public String removedValue() {
+        return null;
+    }
+
+    @Override
+    public String getNotEnoughMoney() {
+        return null;
+    }
+
+    @Override
+    public String inputChat() {
+        return null;
+    }
+
+    @Override
+    public String completed() {
+        return null;
+    }
+
+    @Override
+    public String lostMoney() {
+        return null;
+    }
+
+    @Override
+    public String descConjure() {
+        return null;
+    }
+
+    @Override
+    public String descComplete() {
+        return null;
+    }
+}
