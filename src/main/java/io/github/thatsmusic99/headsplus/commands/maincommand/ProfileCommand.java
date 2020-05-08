e5ef3ebc8cbcc245f3f11ecadc334ed96baa91f5
Thatsmusic99 <tmtherobotcat@gmail.com>
2020-05-08 11:33:57 +0100

    Fixed profiles not being specific in locale

--- src/main/java/io/github/thatsmusic99/headsplus/commands/maincommand/ProfileCommand.java
+++ src/main/java/io/github/thatsmusic99/headsplus/commands/maincommand/ProfileCommand.java
@@ -33,7 +33,7 @@ public class ProfileCommand implements IHeadsPlusCommand {
             return HeadsPlusConfigTextMenu.ProfileTranslator.translate(pl, sender);
         } catch (NullPointerException ex) {
             ex.printStackTrace();
-            return HeadsPlus.getInstance().getMessagesConfig().getString("commands.errors.no-data");
+            return HeadsPlus.getInstance().getMessagesConfig().getString("commands.errors.no-data", sender);
         }
     }
 
