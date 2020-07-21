7309609b676a5fe74f6730c806fe53ecab4260c7
Thatsmusic99 <tmtherobotcat@gmail.com>
2020-07-21 10:13:25 +0100

    Fixed Paper spamming the crap out of the console

--- src/main/java/io/github/thatsmusic99/headsplus/util/events/HeadsPlusTimingsHandler.java
+++ src/main/java/io/github/thatsmusic99/headsplus/util/events/HeadsPlusTimingsHandler.java
@@ -2,28 +2,21 @@ package io.github.thatsmusic99.headsplus.util.events;
 
 import co.aikar.timings.Timings;
 import io.github.thatsmusic99.headsplus.HeadsPlus;
+import io.papermc.lib.PaperLib;
 import org.spigotmc.CustomTimingsHandler;
 
 public class HeadsPlusTimingsHandler {
 
-    private static boolean paper = false;
-    private String name;
+    private final String name;
     private CustomTimingsHandler handler;
 
     public HeadsPlusTimingsHandler(String name) {
         this.name = name;
-    }
 
-    public static void init() {
-        HeadsPlus hp = HeadsPlus.getInstance();
-        if (hp.getServer().getName().equals("Paper")) {
-            paper = true;
-            hp.getLogger().info("Successfully hooked with Paper!");
-        }
     }
 
     public void start() {
-        if (paper) {
+        if (PaperLib.isPaper()) {
             if (Timings.isTimingsEnabled()) {
                 Timings.of(HeadsPlus.getInstance(), name).startTiming();
             }
@@ -34,7 +27,7 @@ public class HeadsPlusTimingsHandler {
     }
 
     public void finish() {
-        if (paper) {
+        if (PaperLib.isPaper()) {
             if (Timings.isTimingsEnabled()) {
                 Timings.of(HeadsPlus.getInstance(), name).stopTiming();
             }
