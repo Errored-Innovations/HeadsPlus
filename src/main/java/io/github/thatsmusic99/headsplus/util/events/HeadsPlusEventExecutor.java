c23ffca3e43477a9c018447f36c4e18bf95f59ea
Thatsmusic99 <tmtherobotcat@gmail.com>
2020-07-22 10:11:43 +0100

    Create a debug report when events fail

--- src/main/java/io/github/thatsmusic99/headsplus/util/events/HeadsPlusEventExecutor.java
+++ src/main/java/io/github/thatsmusic99/headsplus/util/events/HeadsPlusEventExecutor.java
@@ -1,11 +1,14 @@
 package io.github.thatsmusic99.headsplus.util.events;
 
+import io.github.thatsmusic99.headsplus.HeadsPlus;
 import io.github.thatsmusic99.headsplus.util.DebugFileCreator;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventException;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.EventExecutor;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.logging.Logger;
 
 public class HeadsPlusEventExecutor implements EventExecutor {
 
@@ -29,9 +32,17 @@ public class HeadsPlusEventExecutor implements EventExecutor {
                     hpListener.onEvent(event);
                     handler.finish();
                 } catch (Exception ex) {
+                    ex.printStackTrace();
                     HeadsPlusException ex1 = new HeadsPlusException(ex, hpListener.getData());
-                    DebugFileCreator.createReport(ex1);
+                    String s = DebugFileCreator.createReport(ex1);
                     handler.finish();
+                    Logger log = HeadsPlus.getInstance().getLogger();
+                    log.severe("HeadsPlus has failed to execute this task. An error report has been made in /plugins/HeadsPlus/debug");
+                    log.severe("Report name: " + s);
+                    log.severe("Please submit this report to the developer at one of the following links:");
+                    log.severe("https://github.com/Thatsmusic99/HeadsPlus/issues");
+                    log.severe("https://discord.gg/nbT7wC2");
+                    log.severe("https://www.spigotmc.org/threads/headsplus-1-8-x-1-12-x.237088/");
                 }
             }
         }
