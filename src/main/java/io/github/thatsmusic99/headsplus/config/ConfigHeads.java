a83897d824a9ea6b12b009efc5d9c0745bc005af
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2021-04-07 20:53:48 +0100

    Added animations.yml, heads.yml and masks.yml ;))

---
+++ src/main/java/io/github/thatsmusic99/headsplus/config/ConfigHeads.java
@@ -0,0 +1,19 @@
+package io.github.thatsmusic99.headsplus.config;
+
+import io.github.thatsmusic99.configurationmaster.CMFile;
+import org.bukkit.plugin.Plugin;
+
+public class ConfigHeads extends CMFile {
+
+    public ConfigHeads(Plugin plugin) {
+        super(plugin, "heads");
+    }
+
+    @Override
+    public void loadDefaults() {
+        addComment("This is the config where entirely custom heads can be made, with custom metadata, actions, etc.\n" +
+                "To reference a custom head, use HP~head_id.\n" +
+                "If you're looking for mobs.yml instead to change mob drops, please go there :)\n" +
+                "This is different from customheads.yml since only the texture ");
+    }
+}
