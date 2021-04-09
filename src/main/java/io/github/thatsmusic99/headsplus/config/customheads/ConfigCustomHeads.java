a549912d48d362be17af22ba675076d3f79c91ce
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2021-04-09 12:46:43 +0100

    Moved some stuff to AutograbManager

--- src/main/java/io/github/thatsmusic99/headsplus/config/customheads/ConfigCustomHeads.java
+++ src/main/java/io/github/thatsmusic99/headsplus/config/customheads/ConfigCustomHeads.java
@@ -98,112 +98,11 @@ public class ConfigCustomHeads extends CMFile {
         for (String key : getConfig().getConfigurationSection("sections").getKeys(false)) {
             addDefault("sections." + key + ".enabled", true);
             addDefault("sections." + key + ".permission", "headsplus.section." + key);
-
         }
     }
 
-    private void loadHeadsX() {
-        getConfig().options().header("HeadsPlus by Thatsmusic99 "
-                + "\n WARNING: This is an advanced section of the plugin. If you do not know what you a doing with it, please do not use it due to risk of crashing your own and other's games. "
-                + "\n For more information visit the GitHub wiki for customheads.yml: https://github.com/Thatsmusic99/HeadsPlus/wiki/customheads.yml");
-        getConfig().addDefault("options.update-heads", true);
-        getConfig().addDefault("options.version", cVersion);
-        getConfig().addDefault("options.default-price", 10.00);
-        getConfig().addDefault("options.price-per-world.example-one", 15.00);
-        for (HeadsXSections h : HeadsXSections.values()) {
-            getConfig().addDefault("sections." + h.let + ".display-name", h.dn);
-            getConfig().addDefault("sections." + h.let + ".texture", h.tx);
-        }
-        for (HeadsXEnums e : HeadsXEnums.values()) {
-            // getConfig().addDefault("heads." + e.name + ".database", true);
-            getConfig().addDefault("heads." + e.name + ".encode", true);
-            getConfig().addDefault("heads." + e.name + ".displayname", e.dn);
-            getConfig().addDefault("heads." + e.name + ".texture", e.tex);
-            getConfig().addDefault("heads." + e.name + ".price", "default");
-            getConfig().addDefault("heads." + e.name + ".section", e.sec);
-            getConfig().addDefault("heads." + e.name + ".interact-name", e.interactName);
-        }
-
-        getConfig().options().copyDefaults(true);
-        save(false);
-        initCategories();
-    }
-
-    public String getDefaultPath() {
-        return "options.default-price";
-    }
-
-    public void reloadC() {
-      /*  if (configF == null) {
-            File oldFile = new File(HeadsPlus.getInstance().getDataFolder(), "headsx.yml");
-            File newFile = new File(HeadsPlus.getInstance().getDataFolder(), "customheads.yml");
-            if (oldFile.exists()) {
-                try {
-                    Files.copy(oldFile, newFile);
-                    oldFile.delete();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            configF = newFile;
-        }
-        performFileChecks(); */
-        getConfig().addDefault("options.update-heads", true);
-        getConfig().addDefault("options.version", cVersion);
-        getConfig().addDefault("options.default-price", 10.00);
-        getConfig().addDefault("options.price-per-world.example-one", 15.00);
-
-        boolean b = getConfig().getBoolean("options.update-heads");
-        if (getConfig().getDouble("options.version") < cVersion && b) {
-            for (String str : getConfig().getConfigurationSection("heads").getKeys(false)) {
-                getConfig().addDefault("heads." + str + ".price", "default");
-            }
-            getConfig().set("options.version", cVersion);
-            for (String str : getConfig().getConfigurationSection("heads").getKeys(false)) {
-                for (HeadsXEnums e : HeadsXEnums.values()) {
-                    if (e.name.equalsIgnoreCase(str)) {
-                        getConfig().addDefault("heads." + e.name + ".section", e.sec);
-                    }
-                }
-            }
-            for (HeadsXSections h : HeadsXSections.values()) {
-                if (h.d >= cVersion) {
-                    getConfig().addDefault("sections." + h.let + ".display-name", h.dn);
-                    getConfig().addDefault("sections." + h.let + ".texture", h.tx);
-                }
-            }
-            for (HeadsXEnums e : HeadsXEnums.values()) {
-                if (e.v >= cVersion) {
-                    //getConfig().addDefault("heads." + e.name + ".database", true); // isn't actually a required field
-                    getConfig().addDefault("heads." + e.name + ".encode", true);
-                    getConfig().addDefault("heads." + e.name + ".displayname", e.dn);
-                    getConfig().addDefault("heads." + e.name + ".texture", e.tex);
-                    getConfig().addDefault("heads." + e.name + ".price", "default");
-                    getConfig().addDefault("heads." + e.name + ".section", e.sec);
-                    getConfig().addDefault("heads." + e.name + ".interact-name", e.interactName);
-                }
-                if (getConfig().getConfigurationSection("heads." + e.name) != null) {
-                    getConfig().addDefault("heads." + e.name + ".interact-name", e.interactName);
-                }
-            }
-        }
-        getConfig().options().copyDefaults(true);
-        save(true);
-        initCategories();
-    }
-
-    public static ConfigCustomHeads get() {
-        return instance;
-    }
-
-    private void headsxEnable() {
-        reloadC();
-       // if (s) {
-        //      loadHeadsX();
-        //  }
-    }
-
-    private void initCategories() {
+    @Override
+    public void postSave() {
         sections.clear();
         for (String cat : getConfig().getConfigurationSection("sections").getKeys(false)) {
             sections.put(cat, new ArrayList<>());
@@ -224,8 +123,11 @@ public class ConfigCustomHeads extends CMFile {
         } catch (RuntimeException ex) {
             HeadsPlus.getInstance().getLogger().log(Level.SEVERE, "Failed to init skull database", ex);
             sections.clear();
-            return;
         }
+    }
+
+    public static ConfigCustomHeads get() {
+        return instance;
     }
 
     public boolean isHPXSkull(String str) {
@@ -239,9 +141,9 @@ public class ConfigCustomHeads extends CMFile {
             ItemStack is = headsCache.get(s);
             // todo? allow loading texture directly from parameter if matches base64 pattern?
             return is != null ? is.clone() : getSkullFromTexture(
-                    getConfig().getString("heads." + key + ".texture"),
-                    getConfig().getBoolean("heads." + key + ".encode"),
-                    getConfig().getString("heads." + key + ".displayname"));
+                    getString("heads." + key + ".texture"),
+                    getBoolean("heads." + key + ".encode"),
+                    getString("heads." + key + ".displayname"));
         } catch (ArrayIndexOutOfBoundsException ex) {
             HeadsPlus.getInstance().getLogger().severe("An empty ID was found when fetching a head! Please check your customheads.yml configuration or send it to the developer.");
             return null;
@@ -325,204 +227,12 @@ public class ConfigCustomHeads extends CMFile {
         return is;
     }
 
-    public void grabTexture(OfflinePlayer player, boolean force, CommandSender sender) {
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                final String[] playerInfo = new String[1];
-                try {
-                    playerInfo[0] = NBTManager.getProfile(player).getProperties().get("textures").iterator().next().getValue();
-                    addTexture(playerInfo[0], force, sender, player);
-                } catch (NoSuchElementException exception) {
 
-                }
-            }
-        }.runTask(HeadsPlus.getInstance());
 
-    }
 
-    private void addTexture(String info, boolean force, CommandSender sender, OfflinePlayer player) {
-        try {
-            JSONObject playerJson = (JSONObject) new JSONParser().parse(new String(Base64.getDecoder().decode(info.getBytes())));
-            JSONObject textureJson = (JSONObject) playerJson.get("textures");
-            if (textureJson.isEmpty()) return;
-            JSONObject skinJSON = ((JSONObject)textureJson.get("SKIN"));
-            String texture = String.valueOf(skinJSON.get("url"));
-            ConfigurationSection section = HeadsPlus.getInstance().getConfig().getConfigurationSection("plugin.autograb");
-            // If the head never existed
-            if(!allHeadsCache.contains(texture)) {
-                addHead(texture, true,
-                        section.getString("title").replace("{player}", player.getName()),
-                        section.getString("section"),
-                        section.getString("price"),
-                        force || section.getBoolean("add-as-enabled"));
 
-            } else if (force && enableHead(texture)){
-                // Keep going.
-            } else if(sender != null) {
-                HeadsPlusMessagesManager.get().sendMessage("commands.addhead.head-already-added", sender, "{player}", player.getName());
-                return;
-            }
-            if(sender != null) {
-                HeadsPlusMessagesManager.get().sendMessage("commands.addhead.head-added", sender, "{player}", player.getName());
-            }
 
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
 
-    public void grabProfile(String id) {
-        grabProfile(id, null, false);
-    }
-
-    // texture lookups need to be protected from spam
-    HashMap<String, Long> lookups = new HashMap<>();
-
-    public boolean grabProfile(String id, CommandSender callback, boolean forceAdd) {
-        Long last = lookups.get(id);
-        long now = System.currentTimeMillis();
-        if(last != null && last > now - 180000) {
-            if(callback != null) {
-                callback.sendMessage(ChatColor.RED + "/addhead spam protection - try again in a few minutes");
-            }
-            return false;
-        } else {
-            lookups.put(id, now);
-        }
-        grabProfile(id, 3, callback, forceAdd, forceAdd ? 5 : 20 * 20);
-        return true;
-    }
-
-    public String grabUUID(String username, int tries, CommandSender callback) {
-        String uuid = null;
-        BufferedReader reader = null;
-        try {
-            URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + username);
-            reader = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream(), StandardCharsets.UTF_8));
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if(sb.length() == 0) {
-                    sb.append("\n");
-                }
-                sb.append(line);
-            }
-            String json = sb.toString();
-            JSONObject resp = (JSONObject) JSONValue.parse(json);
-            if(resp == null || resp.isEmpty()) {
-                HeadsPlus.getInstance().getLogger().warning("Failed to grab data for user " + username + " - invalid username.");
-                if(callback != null) {
-                    callback.sendMessage(ChatColor.RED + "Error: Failed to grab data for user " + username + "!");
-                }
-                return null;
-            } else if(resp.containsKey("error")) {
-                // Retry
-                if(tries > 0) {
-                    grabUUID(username, tries - 1, callback);
-                } else if(callback != null) {
-                    callback.sendMessage(ChatColor.RED + "Error: Failed to grab data for user " + username + "!");
-                }
-                return null;
-            } else {
-                uuid = String.valueOf(resp.get("id")); // Trying to parse this as a UUID will cause an IllegalArgumentException
-            }
-        } catch (IOException e) {
-            DebugPrint.createReport(e, "Retreiving UUID (addhead)", true, callback);
-        }
-        return uuid;
-    }
-
-    protected void grabProfile(String id, int tries, CommandSender callback, boolean forceAdd, int delay) {
-        Bukkit.getScheduler().runTaskLaterAsynchronously(HeadsPlus.getInstance(), () -> {
-                    BufferedReader reader = null;
-            try {
-                if (id == null) return;
-                URL uRL = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + id.replace("-", ""));
-
-                reader = new BufferedReader(new InputStreamReader(uRL.openConnection().getInputStream(), StandardCharsets.UTF_8));
-                StringBuilder sb = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    if(sb.length() == 0) {
-                        sb.append("\n");
-                    }
-                    sb.append(line);
-                }
-                String json = sb.toString();
-
-                JSONObject resp = (JSONObject) JSONValue.parse(json);
-                if(resp == null || resp.isEmpty()) {
-                    HeadsPlus.getInstance().getLogger().warning("Failed to grab data for user " + id + " - invalid id");
-                    if(callback != null) {
-                        callback.sendMessage(ChatColor.RED + "Error: Failed to grab data for user " + Bukkit.getOfflinePlayer(id).getName());
-                    }
-                    return;
-                } else if(resp.containsKey("error")) {
-                    // retry
-                    if(tries > 0) {
-                        grabProfile(id, tries - 1, callback, forceAdd, 30 * 20);
-                    } else if(callback != null) {
-                        callback.sendMessage(ChatColor.RED + "Error: Failed to grab data for user " + Bukkit.getOfflinePlayer(id).getName());
-                    }
-                    return;
-                }
-
-                Object o = resp.get("properties");
-                if(o instanceof List) {
-                    for(Object o2 : (List) o) {
-                        if(o2 instanceof Map) {
-                           Map m = (Map) o2;
-                           if("textures".equals(m.get("name")) && m.containsKey("value")) {
-                               String encoded = m.get("value").toString();
-                               String decoded = new String(Base64.getDecoder().decode(encoded));
-                               JSONObject resp2 = (JSONObject) JSONValue.parse(decoded);
-                               if((o2 = resp2.get("textures")) instanceof Map
-                                       && (o2 = ((Map) o2).get("SKIN")) instanceof Map
-                                       && ((Map) o2).containsKey("url")) {
-                                   String texUrl = ((Map) o2).get("url").toString();
-                                   int last = texUrl.lastIndexOf('/');
-                                   if(last != -1) {
-                                       texUrl = texUrl.substring(last + 1);
-                                       String name = resp.get("name").toString();
-                                       if(!allHeadsCache.contains(texUrl)) {
-                                           addHead(texUrl, true,
-                                                   HeadsPlus.getInstance().getConfig().getString("plugin.autograb.title").replace("{player}", name),
-                                                   HeadsPlus.getInstance().getConfig().getString("plugin.autograb.section"), 
-                                                   HeadsPlus.getInstance().getConfig().getString("plugin.autograb.price"), 
-                                                   forceAdd || HeadsPlus.getInstance().getConfig().getBoolean("plugin.autograb.add-as-enabled"));
-                                            if(callback != null) {
-                                                HeadsPlusMessagesManager.get().sendMessage("commands.addhead.head-added", callback, "{player}", name);
-                                            }
-                                       } else if (forceAdd && enableHead(texUrl)){
-                                           if(callback != null) {
-                                               HeadsPlusMessagesManager.get().sendMessage("commands.addhead.head-added", callback, "{player}", name);
-                                            }
-                                       } else if(callback != null) {
-                                           HeadsPlusMessagesManager.get().sendMessage("commands.addhead.head-already-added", callback, "{player}", name);
-                                       }
-                                   }
-                               }
-                           }
-                        }
-                    }
-                }
-            } catch (Exception ex) {
-                if(ex instanceof IOException && ex.getMessage().contains("Server returned HTTP response code: 429 for URL")) {
-                    grabProfile(id, tries - 1, callback, forceAdd, 30 * 20);
-                } else {
-                    DebugPrint.createReport(ex, "Retreiving profile (addhead)", true, callback);
-                }
-            } finally {
-                if(reader != null) {
-                    try {
-                        reader.close();
-                    } catch (IOException ex) {
-                    }
-                }
-            }
-        }, delay);
-    }
 
     /**
      * Enable player head texture, if not enabled already
@@ -586,7 +296,7 @@ public class ConfigCustomHeads extends CMFile {
     void delaySave() {
         if (autosaveTask == -1 && HeadsPlus.getInstance().isEnabled()) {
             autosaveTask = Bukkit.getScheduler().runTaskLaterAsynchronously(HeadsPlus.getInstance(), ()->{
-                save(true);
+                initiateSave();
                 autosaveTask = -1;
             }, 5 * 60).getTaskId();
         }
@@ -595,7 +305,7 @@ public class ConfigCustomHeads extends CMFile {
     public void flushSave() {
         if (autosaveTask != -1) {
             Bukkit.getScheduler().cancelTask(autosaveTask);
-            save(true);
+            initiateSave();
             autosaveTask = -1;
         }
     }
