891b0a0f33b5b8e887a818459e0c888c007bbccd
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2021-05-16 19:37:01 +0100

    Added zh_TW (courtesy of andy3559167, thank you!)

--- src/main/java/io/github/thatsmusic99/headsplus/config/HeadsPlusConfigTextMenu.java
+++ src/main/java/io/github/thatsmusic99/headsplus/config/HeadsPlusConfigTextMenu.java
@@ -411,7 +411,7 @@ public class HeadsPlusConfigTextMenu extends ConfigSettings {
                         .replace("{header}", h.getConfig().getString("info.header"))
                         .replace("{author}", String.valueOf(hp.getAuthor()))
                         .replace("{locale}", hp.getConfiguration().getConfig().getString("locale"))
-                        .replaceAll("\\{contributors}", "Toldi, DariusTK, AlansS53, Gneiwny, steve4744, Niestrat99, Alexisparis007, jascotty2, Gurbiel, Mistermychciak, stashenko/The_stas, YouHaveTrouble, Tepoloco, Bieck_Smile, PaulBGD"), sender));
+                        .replaceAll("\\{contributors}", "Toldi, DariusTK, AlansS53, Gneiwny, steve4744, Niestrat99, Alexisparis007, jascotty2, Gurbiel, Mistermychciak, stashenko/The_stas, YouHaveTrouble, Tepoloco, Bieck_Smile, PaulBGD, andy3559167"), sender));
             }
             return sb.toString();
         }
