a79fd033f2215631de5353fad37a323d786e9b41
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2021-06-18 17:12:48 +0100

    Implemented more of the crafting manager

--- src/main/java/io/github/thatsmusic99/headsplus/managers/CraftingManager.java
+++ src/main/java/io/github/thatsmusic99/headsplus/managers/CraftingManager.java
@@ -1,15 +1,47 @@
 package io.github.thatsmusic99.headsplus.managers;
 
+import io.github.thatsmusic99.headsplus.config.ConfigCrafting;
+import io.github.thatsmusic99.headsplus.util.HPUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.Recipe;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class CraftingManager {
 
-    private HashMap<String, Recipe> recipes;
+    private HashMap<NamespacedKey, Recipe> recipes;
     private static CraftingManager instance;
 
     public CraftingManager() {
         instance = this;
+        recipes = new LinkedHashMap<>();
+    }
+
+    public void init() {
+        clear();
+        ConfigCrafting crafting = ConfigCrafting.get();
+        FileConfiguration actualConfig = crafting.getConfig();
+    }
+
+    public void clear() {
+        for (NamespacedKey key : recipes.keySet()) {
+            Bukkit.removeRecipe(key);
+        }
+    }
+
+    public void addRecipe(String key, ConfigurationSection section) {
+        // Get the result section
+        ConfigurationSection resultSection = section.getConfigurationSection("result");
+        HPUtils.notNull(resultSection, "Recipe " + key + " does not have a result section!");
+
+    }
+
+    public static CraftingManager get() {
+        return instance;
     }
 }
