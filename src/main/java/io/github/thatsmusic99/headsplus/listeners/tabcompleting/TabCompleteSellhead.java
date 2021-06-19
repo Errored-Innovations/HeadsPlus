f6606a8022759905b18feb08c90d83be414d66b6
YouHaveTrouble <garrenpolska@gmail.com>
2021-06-19 14:37:12 +0200

    made this readable smh also added vanish bypass fix

--- src/main/java/io/github/thatsmusic99/headsplus/listeners/tabcompleting/TabCompleteSellhead.java
+++ src/main/java/io/github/thatsmusic99/headsplus/listeners/tabcompleting/TabCompleteSellhead.java
@@ -6,6 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.metadata.MetadataValue;
 import org.bukkit.util.StringUtil;
 
 import java.util.ArrayList;
@@ -14,19 +15,25 @@ import java.util.List;
 public class TabCompleteSellhead implements TabCompleter {
 
     @Override
-    public List<String> onTabComplete(CommandSender cs, Command command, String s, String[] args) {
+    public List<String> onTabComplete(CommandSender cs, Command command, String alias, String[] args) {
         if (args.length == 1) {
-            List<String> c = new ArrayList<>();
-            c.addAll(SellHead.getRegisteredIDs());
-            c.add("all");
-            List<String> f = new ArrayList<>();
-            StringUtil.copyPartialMatches(args[0], c, f);
-            return f;
+            List<String> headIdList = new ArrayList<>(SellHead.getRegisteredIDs());
+            headIdList.add("all");
+            List<String> suggestions = new ArrayList<>();
+            StringUtil.copyPartialMatches(args[0], headIdList, suggestions);
+            return suggestions;
         }
-        List<String> p = new ArrayList<>();
-        for (Player pl : Bukkit.getOnlinePlayers()) {
-            p.add(pl.getName());
+        List<String> players = new ArrayList<>();
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (!isVanished(player)) players.add(player.getName());
         }
-        return p;
+        return players;
+    }
+
+    private boolean isVanished(Player player) {
+        for (MetadataValue meta : player.getMetadata("vanished")) {
+            if (meta.asBoolean()) return true;
+        }
+        return false;
     }
 }
