094e88bdd7297d441f149428d045adfdd171d6dc
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2021-11-23 19:58:34 +0000

    Added version checking that's actually good

--- src/main/java/io/github/thatsmusic99/headsplus/UpdateChecker.java
+++ src/main/java/io/github/thatsmusic99/headsplus/UpdateChecker.java
@@ -10,6 +10,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
 
 @Deprecated // should probably replace regardless lmao
 class UpdateChecker {
@@ -19,19 +22,22 @@ class UpdateChecker {
     private final static String versionURL = "https://api.spiget.org/v2/resources/40265/versions/latest";
 
     static Object[] getUpdate() {
-        HeadsPlus hp = HeadsPlus.get();
         try {
-            JSONObject results = getURLResults(versionURL);
-            if (results == null) return null;
-            String version = (String) results.get("name");
-            if (version.equals(hp.getDescription().getVersion())) return null;
-            JSONObject updateInfo = getURLResults(updateURL);
-            if (updateInfo == null) return null;
-            String title = (String) updateInfo.get("title");
-            if (title == null) return null;
-            return new Object[]{version, title};
-        } catch (ParseException | IOException e) {
+            JSONObject latestVersionObj = getURLResults(versionURL);
+            if (latestVersionObj == null) return null;
+            String newVersion = (String) latestVersionObj.get("name");
+            // we are a little stupid
+            if (newVersion.equals(HeadsPlus.get().getDescription().getVersion())) return null;
+            long latestTimestamp = (long) latestVersionObj.get("releaseDate") * 1000;
+            if (latestTimestamp <= getInternalTimestamp()) return null;
+            JSONObject updateDesc = getURLResults(updateURL);
+            String updateName = (String) updateDesc.get("title");
+            return new Object[]{newVersion, updateName};
+        } catch (ParseException | java.text.ParseException e) {
             e.printStackTrace();
+            return null;
+        } catch (IOException ex) {
+            HeadsPlus.get().getLogger().severe("Failed to get plugin update information, is Spiget down?");
             return null;
         }
     }
@@ -39,13 +45,18 @@ class UpdateChecker {
     private static JSONObject getURLResults(String urlStr) throws IOException, ParseException {
         URL url = new URL(urlStr);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.addRequestProperty("User-Agent", "HeadsPlusPluginAgent");
-        try {
-            InputStream inputStream = connection.getInputStream();
-            InputStreamReader reader = new InputStreamReader(inputStream);
-            return (JSONObject) new JSONParser().parse(reader);
-        } catch (FileNotFoundException ex) {
-            return null;
-        }
+        connection.addRequestProperty("User-Agent", "HeadsPlusPA");
+        InputStream inputStream = connection.getInputStream();
+        InputStreamReader reader = new InputStreamReader(inputStream);
+        return (JSONObject) new JSONParser().parse(reader);
+    }
+
+    private static long getInternalTimestamp() throws IOException, java.text.ParseException {
+        InputStream updateStream = HeadsPlus.class.getResourceAsStream("/update.properties");
+        Properties updateProperties = new Properties();
+        updateProperties.load(updateStream);
+        String timestamp = (String) updateProperties.get("update-timestamp");
+        Date date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse(timestamp);
+        return date.getTime();
     }
 }
