48381cddf8d9b111d8603df81c28a1d34fcbfbc7
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2022-05-07 23:55:59 +0100

    Cancel InventoryMoveItemEvent

--- src/main/java/io/github/thatsmusic99/headsplus/inventories/BaseInventory.java
+++ src/main/java/io/github/thatsmusic99/headsplus/inventories/BaseInventory.java
@@ -20,6 +20,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
@@ -166,6 +167,12 @@ public abstract class BaseInventory implements InventoryHolder, Listener {
     @Override
     public Inventory getInventory() {
         return inventory;
+    }
+
+    @EventHandler
+    public void onItemMove(InventoryMoveItemEvent event) {
+        if (event.getDestination().getHolder() != this) return;
+        event.setCancelled(true);
     }
 
     @EventHandler
