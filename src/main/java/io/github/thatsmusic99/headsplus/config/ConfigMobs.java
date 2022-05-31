ae8a4e627be5f193f7e43b0044505831ec1df72d
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2022-05-31 20:25:16 +0100

    Add proper lore detection

--- src/main/java/io/github/thatsmusic99/headsplus/config/ConfigMobs.java
+++ src/main/java/io/github/thatsmusic99/headsplus/config/ConfigMobs.java
@@ -308,7 +308,7 @@ public class ConfigMobs extends FeatureConfig {
     @Nullable
     public List<String> getLore(String type, String conditions, String name, double price, String killerName) {
         List<String> lore = new ArrayList<>();
-        List<String> configLore = getList(type + "." + conditions + ".lore", getList("defaults.lore", null));
+        List<String> configLore = getList(type + "." + conditions + "." + name + ".lore", getList("defaults.lore", null));
         if (configLore == null) return null;
         for (String l : configLore) {
             HPUtils.parseLorePlaceholders(lore, ChatColor.translateAlternateColorCodes('&', l),
