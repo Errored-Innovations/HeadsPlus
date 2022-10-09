e52f40136c4cd7fc5d9c9c5d89d755251094594b
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2022-10-09 12:11:20 +0100

    Add HP# head to the item reward

--- src/main/java/io/github/thatsmusic99/headsplus/api/rewards/ItemReward.java
+++ src/main/java/io/github/thatsmusic99/headsplus/api/rewards/ItemReward.java
@@ -5,6 +5,7 @@ import io.github.thatsmusic99.headsplus.HeadsPlus;
 import io.github.thatsmusic99.headsplus.api.Challenge;
 import io.github.thatsmusic99.headsplus.api.Reward;
 import io.github.thatsmusic99.headsplus.config.MessagesManager;
+import io.github.thatsmusic99.headsplus.managers.HeadManager;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -13,18 +14,37 @@ public class ItemReward extends Reward {
 
     private final ItemStack item;
 
-    public ItemReward(Material material, int amount, long xp) {
+    public ItemReward(ItemStack item, long xp) {
         super(xp);
-        item = new ItemStack(material, amount);
+        this.item = item;
+    }
+
+    @Deprecated
+    public ItemReward(Material material, int amount, long xp) {
+        this(new ItemStack(material, amount), xp);
     }
 
     public static ItemReward fromConfigSection(String id, ConfigSection section) {
         String materialStr = section.getString("material");
         if (materialStr == null)
             throw new IllegalStateException("Reward type GIVE_ITEM " + id + " must have a material option!");
-        Material material = Material.getMaterial(materialStr);
-        int amount = section.getInteger("amount", 1);
-        return new ItemReward(material, amount, section.getLong("base-xp"));
+        // If it's a mask or HP head...
+        if (materialStr.startsWith("HP#") || materialStr.startsWith("HPM#")) {
+            // Get the head in question
+            HeadManager.HeadInfo headInfo = HeadManager.get().getHeadInfo(materialStr);
+            int amount = section.getInteger("amount", 1);
+            ItemStack itemStack = headInfo.forceBuildHead();
+            itemStack.setAmount(amount);
+            return new ItemReward(itemStack, section.getLong("base-xp"));
+        } else {
+            Material material = Material.getMaterial(materialStr);
+            if (material == null) {
+                throw new IllegalStateException("Reward type GIVE_ITEM " + id + " was given a material " + materialStr + " that was not found!");
+            }
+            int amount = section.getInteger("amount", 1);
+            return new ItemReward(new ItemStack(material, amount), section.getLong("base-xp"));
+        }
+
     }
 
     @Override
