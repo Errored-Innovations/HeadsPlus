797f78459bb8e36d8570e0107d07e2c8e7fa8b14
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2025-12-23 13:27:19 +0100

    fix: wolf head drops not working

--- src/main/java/io/github/thatsmusic99/headsplus/managers/EntityDataManager.java
+++ src/main/java/io/github/thatsmusic99/headsplus/managers/EntityDataManager.java
@@ -150,6 +150,7 @@ public class EntityDataManager {
             }
             case "WOLF": {
                 builder.append(getUnstableKeyed(entity, "getVariant", "PALE"));
+                break;
             }
             case "COW":
             case "PIG":
